8fd921a5b944032d3b3a2351be0b00520d299b57
gcrtnst <52910071+gcrtnst@users.noreply.github.com>
2019-05-07 00:52:28 +0900

    Add niconico.utils.parse_id

--- niconico/utils.py
+++ niconico/utils.py
@@ -3,17 +3,23 @@ import re
 from .exceptions import InvalidContentID
 
 
-def int_id(prefix, content_id):
+def parse_id(content_id):
     if isinstance(content_id, int):
-        return content_id
+        return (None, content_id)
     elif isinstance(content_id, str):
         match = re.search(r'^(?P<prefix>[a-z]+)?(?P<id>[0-9]+)$', content_id)
         if match:
-            match_prefix = match.group('prefix')
-            if match_prefix and match_prefix != prefix:
-                raise InvalidContentID('expected "' + prefix + '" for content id prefix, found "' + match_prefix + '"')
-            return int(match.group('id'))
+            groupdict = match.groupdict()
+            if groupdict['id']:
+                return (groupdict['prefix'], int(groupdict['id']))
     raise InvalidContentID('invalid context id: {}'.format(content_id))
+
+
+def int_id(prefix, content_id):
+    p, i = parse_id(content_id)
+    if p is not None and p != prefix:
+        raise InvalidContentID('expected "' + prefix + '" for content id prefix, found "' + p + '"')
+    return i
 
 
 def str_id(prefix, content_id):
