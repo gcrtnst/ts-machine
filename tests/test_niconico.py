07669679876b405de0a8913af3de42f0d4b5483f
gcrtnst <52910071+gcrtnst@users.noreply.github.com>
2019-06-01 11:29:59 +0900

    Add test for niconico.client._login_if_required

--- tests/test_niconico.py
+++ tests/test_niconico.py
@@ -1,7 +1,47 @@
 import unittest
+from unittest.mock import Mock, call
 
+import niconico.client
 import niconico.utils
-from niconico.exceptions import InvalidContentID
+from niconico.client import Niconico
+from niconico.exceptions import InvalidContentID, LoginFailed, LoginRequired
+
+
+class TestNiconico(unittest.TestCase):
+    def test_login_if_required(self):
+        n = Mock(spec_set=Niconico())
+        n.mail = None
+        n.password = None
+        func = Mock()
+        niconico.client._login_if_required(func)(n)
+        self.assertEqual(n.mock_calls, [])
+        self.assertEqual(func.mock_calls, [call(n)])
+
+        n = Mock(spec_set=Niconico())
+        n.mail = None
+        n.password = None
+        func = Mock(side_effect=LoginRequired)
+        with self.assertRaises(LoginRequired):
+            niconico.client._login_if_required(func)(n)
+        self.assertEqual(n.mock_calls, [])
+        self.assertEqual(func.mock_calls, [call(n)])
+
+        n = Mock(spec_set=Niconico())
+        n.mail = 'email@example.com'
+        n.password = 'password'
+        func = Mock(side_effect=[LoginRequired, None])
+        niconico.client._login_if_required(func)(n)
+        self.assertEqual(n.mock_calls, [call.login()])
+        self.assertEqual(func.mock_calls, [call(n), call(n)])
+
+        n = Mock(spec_set=Niconico())
+        n.mail = 'email@example.com'
+        n.password = 'password'
+        func = Mock(side_effect=[LoginRequired, LoginRequired])
+        with self.assertRaises(LoginFailed):
+            niconico.client._login_if_required(func)(n)
+        self.assertEqual(n.mock_calls, [call.login()])
+        self.assertEqual(func.mock_calls, [call(n), call(n)])
 
 
 class TestNiconicoUtils(unittest.TestCase):
